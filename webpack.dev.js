88e9a00c049b80025fc40df5d3555e23ebd46f33
vinhmai <vinhmai079@gmail.com>
2020-11-07 20:51:17 +0700

    update

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,26 @@
 const path = require('path')
 const merge = require('webpack-merge')
 const webpackCommon = require('./webpack.common')
-const webpackTemplates = require('./webpack.templates')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const files = ['horizontal-transform', 'vertical-transform', 'all-transform', 'horizontal-native', 'vertical-native', 'all-native']
 
-module.exports = merge(webpackTemplates, webpackCommon, {
+module.exports = merge(webpackCommon, {
     mode: 'development',
     devtool: 'eval-source-map',
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: ['public/style.css'],
+        }),
+        ...files.map((file) => {
+            return new HtmlWebpackPlugin({
+                filename: `${file}.html`,
+                template: path.resolve(__dirname, `public/${file}.ejs`),
+            })
+        }),
+    ],
     devServer: {
         inline: true,
         hot: true,
