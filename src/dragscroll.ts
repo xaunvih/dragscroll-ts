1f27033ac81cf37ac247cdcde4889c080b39ac82
vinhmx <vinh.mai@axonactive.com>
2020-12-15 13:37:35 +0700

    add bound to 4 edges

--- src/dragscroll.ts
+++ src/dragscroll.ts
@@ -46,7 +46,6 @@ class DragScroll {
         this.onDragEnd = this.onDragEnd.bind(this)
         this.animate = this.animate.bind(this)
         this.bindEvents()
-        this.animate()
     }
 
     bindEvents(): void {
@@ -57,7 +56,10 @@ class DragScroll {
 
     update() {
         this.applyDragForce()
-        this.applyBoundForce()
+        this.applyLeftBoundForce()
+        this.applyRightBoundForce()
+        this.applyTopBoundForce()
+        this.applyBottomBoundForce()
 
         const { position, velocity } = this.state
         const { friction } = this.options
@@ -88,7 +90,125 @@ class DragScroll {
         this.applyForce(dragForce)
     }
 
-    applyBoundForce() {}
+    applyLeftBoundForce() {
+        const { velocity, position, isDragging } = this.state
+        const { friction } = this.options
+        const leftBound = 0
+        const isInside = position.x > leftBound
+        if (isDragging || isInside) {
+            return
+        }
+
+        // bouncing past bound
+        const distance = leftBound - position.x
+        let force = distance * 0.1
+        const restX = position.x + ((velocity.x + force) * friction) / (1 - friction)
+        const isRestOutside = restX < leftBound
+        if (isRestOutside) {
+            this.applyForce({
+                x: force,
+                y: 0
+            })
+
+            return
+        }
+
+        // bounce back
+        this.applyForce({
+            x: distance * 0.1 - velocity.x,
+            y: 0
+        })
+    }
+
+    applyRightBoundForce() {
+        const { velocity, position, isDragging } = this.state
+        const { friction } = this.options
+        const rightBound = this.$container.clientWidth - this.$content.clientWidth - 40
+        const isInside = position.x < rightBound
+        if (isDragging || isInside) {
+            return
+        }
+
+        // bouncing past bound
+        const distance = rightBound - position.x
+        let force = distance * 0.1
+        const restX = position.x + ((velocity.x + force) * friction) / (1 - friction)
+        const isRestOutside = restX > rightBound
+        if (isRestOutside) {
+            this.applyForce({
+                x: force,
+                y: 0
+            })
+
+            return
+        }
+
+        // bounce back
+        this.applyForce({
+            x: distance * 0.1 - velocity.x,
+            y: 0
+        })
+    }
+
+    applyTopBoundForce() {
+        const { velocity, position, isDragging } = this.state
+        const { friction } = this.options
+        const topBound = 0
+        const isInside = position.y < topBound
+        if (isDragging || isInside) {
+            return
+        }
+
+        // bouncing past bound
+        const distance = topBound - position.y
+        let force = distance * 0.1
+        const restY = position.y + ((velocity.y + force) * friction) / (1 - friction)
+        const isRestOutside = restY > topBound
+        if (isRestOutside) {
+            this.applyForce({
+                x: 0,
+                y: force
+            })
+
+            return
+        }
+
+        // bounce back
+        this.applyForce({
+            x: 0,
+            y: distance * 0.1 - velocity.y
+        })
+    }
+
+    applyBottomBoundForce() {
+        const { velocity, position, isDragging } = this.state
+        const { friction } = this.options
+        const bottomBound = 0
+        const isInside = position.y > bottomBound
+        if (isDragging || isInside) {
+            return
+        }
+
+        // bouncing past bound
+        const distance = bottomBound - position.y
+        let force = distance * 0.1
+        const restY = position.y + ((velocity.y + force) * friction) / (1 - friction)
+        const isRestOutside = restY < bottomBound
+        if (isRestOutside) {
+            this.applyForce({
+                x: 0,
+                y: force
+            })
+
+            return
+        }
+
+        // bounce back
+        this.applyForce({
+            x: 0,
+            y: distance * 0.1 - velocity.y
+        })
+    }
 
     onDragStart(evt: MouseEvent): void {
         evt.preventDefault()
@@ -106,6 +226,7 @@ class DragScroll {
         }
 
         this.setDragPosition(evt)
+        this.startAnimation()
     }
 
     onDraging(evt: MouseEvent): void {
